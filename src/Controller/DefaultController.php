7186ca5d103f84c2578c49e24b3f5f6629339716
Aleksandr Matrosov <unglud@gmail.com>
2021-04-28 15:50:38 +0200

    Template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends AbstractController
      */
     public function __construct() { }
 
-    #[Route('/', name: 'default')]
+    #[Route('/default', name: 'default')]
     public function index(
         Request $request, SessionInterface $session, TranslatorInterface $translator
     ): Response {
---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    #[Route('/', name: 'to_do_list')]
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'ToDoListController',
+        ]);
+    }
+}
