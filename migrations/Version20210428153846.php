f57d75815a918911396054c984b1ce3eaeac6e6e
Aleksandr Matrosov <unglud@gmail.com>
2021-04-28 17:54:58 +0200

    Task entity

---
+++ migrations/Version20210428153846.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210428153846 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, status BOOLEAN DEFAULT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE task');
+    }
+}
--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,27 +14,38 @@ class ToDoListController extends AbstractController
     #[Route('/', name: 'to_do_list')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', [
-            'controller_name' => 'ToDoListController',
+        return $this->render("index.html.twig", [
+            "controller_name" => "ToDoListController",
         ]);
     }
 
     #[Route('/create', name: 'create_task', methods: ['POST'])]
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        exit('todo: create new task');
-    }
+        $title = trim($request->request->get("title"));
+        if (empty($title)) {
+            return $this->redirectToRoute("to_do_list");
+        }
 
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = new Task();
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("to_do_list");
+    }
 
     #[Route('/switch-status/{id}', name: 'switch_status')]
     public function switchStatus($id): Response
     {
-        exit('todo: switch-status task'. $id);
+        exit("todo: switch-status task" . $id);
     }
 
     #[Route('/delete/{id}', name: 'delete')]
     public function delete($id): Response
     {
-        exit('todo: delete task'. $id);
+        exit("todo: delete task" . $id);
     }
 }
